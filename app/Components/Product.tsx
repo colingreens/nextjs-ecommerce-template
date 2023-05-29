7f9e0b0d49dd280d3ee76f54e4d48ba175a9aeff
colin_greens <colin.j.benson@gmail.com>
2023-05-28 23:56:20 -0700

    inital styles to home page

--- app/Components/Product.tsx
+++ app/Components/Product.tsx
@@ -4,10 +4,20 @@ import Image from "next/image";
 
 export default function Product({ name, image, price }: AddItemType) {
   return (
-    <div>
-      <Image src={image} alt={name} width={400} height={400} />
-      <h1>{name}</h1>
-      {price ? formatPrice(price) : "N/A"}
+    <div className="text-gray-700">
+      <Image
+        src={image}
+        alt={name}
+        width={400}
+        height={400}
+        className="w-full h-80 object-cover rounded-lg"
+      />
+      <div className="font-medium py-2">
+        <h1>{name}</h1>
+        <h2 className="text-sm text-teal-700">
+          {price ? formatPrice(price) : "N/A"}
+        </h2>
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -26,9 +26,9 @@ export default async function aHome() {
   const products = await getProducts();
 
   return (
-    <main>
+    <main className="grid grid-cols-fluid gap-12">
       {products.map((product) => (
-        <Product {...product} />
+        <Product {...product} key={product.id} />
       ))}
     </main>
   );
